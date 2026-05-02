26e0f27dfa22e6834c582f05b7ca958904c4a562
joao <joaopedroazevedo1980@gmail.com>
2026-05-02 15:07:38 -0300

    FluxogramaDaEngenharia

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+bool estamovendo = Console.ReadLine().Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine().Trim().Substring(0, 1).ToUpper() == "S";
+string Mensagem;
+if (!estamovendo)
+{
+if (!deveria)
+{
+Mensagem = "Otimo!";
+}
+else 
+{
+Mensagem = "Use WD-40.";
+}
+}
+else 
+{
+if (deveria)
+{
+    Mensagem = "Otimo!";
+}
+else
+{
+Mensagem = "Use Silver Tape.";
+}
+}
+
+Console.WriteLine();
+Console.WriteLine(Mensagem);
